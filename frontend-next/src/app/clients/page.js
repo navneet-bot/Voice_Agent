deea798b2a06ec581ea022ae08a715a40e2a29f0
mani <mani@manis-MacBook-Air.local>
2026-04-28 23:51:28 +0530

    updated code

--- frontend-next/src/app/clients/page.js
+++ frontend-next/src/app/clients/page.js
@@ -1,5 +1,5 @@
 'use client';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import DashboardLayout from '@/components/DashboardLayout';
 import { useAuth, clientProfile } from '@/context/AuthContext';
 
